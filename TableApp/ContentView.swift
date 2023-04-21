0e2b8ee2fbe19834db4a4d975afdf3c92f7b495c
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-04-21 17:02:26 +0300

    table in swift ui

--- TableApp/ContentView.swift
+++ TableApp/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var products = Product.getProducts()
+    @State private var sortOrder = [KeyPathComparator(\Product.name)]
+    @State private var selected = Set<Product.ID>()
+    
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    
+    private var isCompact: Bool {
+        horizontalSizeClass == .compact
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Table(products, selection: $selected, sortOrder: $sortOrder) {
+                TableColumn("Name", value: \.name) { product in
+                    VStack {
+                        Text(product.name)
+                        
+                        if isCompact {
+                            Text(product.code.formatted())
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+                 //   .width(150)
+                TableColumn("Category", value: \.category)
+                TableColumn("Code", value: \.code) { Text($0.code.formatted()) }
+                TableColumn("Available", value: \.isAvailableToInt) {
+                    Text($0.isAvailable ? "🟢" : "❌")
+                }
+            }
+            .onChange(of: sortOrder) { newValue in
+                products.sort(using: newValue)
+            }
+            .navigationTitle("Products")
+            .toolbar {
+                ToolbarItem {
+                    EditButton()
+                }
+            }
         }
-        .padding()
     }
 }
 
