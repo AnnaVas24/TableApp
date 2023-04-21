39c8447e3d0541c3770d8bf110ac4d4fc9e49e2c
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-04-21 12:04:13 +0300

    Initial Commit

---
+++ TableApp/TableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableAppApp.swift
+//  TableApp
+//
+//  Created by Vasichko Anna on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
